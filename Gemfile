254582ea206e05bdb5c30d3cc912aa03ae5e994c
michael <muzimike@gmail.com>
2021-12-03 11:51:56 +0200

    problems 1,2,3

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in advent-of-code-2021-ruby.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/advent/of/code/2021/ruby.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "ruby/version"
+
+module Advent
+  module Of
+    module Code
+      module 2021
+        module Ruby
+          class Error < StandardError; end
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
